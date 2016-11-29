89b1982bdd5a2cb497fe97d0db4c10946b94235d
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-11-29 10:53:12 +0100

    update export_transform.py

--- scripts/export_transform.py
+++ scripts/export_transform.py
@@ -8,6 +8,7 @@ import bson
 import datetime
 import json
 import os
+import rospy
 import sys
 from jsk_owl_exporter import *
 
@@ -19,12 +20,17 @@ def json_serializer(o):
         return { "$date": get_epoch_time(o) }
     raise TypeError(repr(o) + " is not JSON serializable")
 
-def export_tf(db_addr, task_id, out_path):
+def export_tf(db_addr, task_id, out_path, srv_name):
     client = get_mongo_client(db_addr)
+    bson_cvt = BSONConversion(srv_name)
     cur = client.find({
         "_meta.stored_type": { "$in": [
             "geometry_msgs/TransformStamped",
-            "posedetection_msgs/Object6DPose" ]},
+            "posedetection_msgs/Object6DPose",
+            "move_base_msgs/MoveBaseActionFeedback",
+            "control_msgs/FollowJointTrajectoryActionFeedback",
+            "sensor_msgs/JointState",
+        ]},
         "_meta.task_id": task_id,
     }).sort("_id")
 
@@ -38,16 +44,14 @@ def export_tf(db_addr, task_id, out_path):
     with open(out_path, "w") as f:
         for d in cur:
             t = d["_meta"]["stored_type"]
-            if t == "posedetection_msgs/Object6DPose":
-                json.dump(object6dpose_to_tf(d), f, default=json_serializer)
-            elif t == "geometry_msgs/TransformStamped":
-                json.dump(transform_stamped_to_tf(d), f, default=json_serializer)
+            json.dump(bson_cvt.to_tf_json(d), f, default=json_serializer)
             f.write(os.linesep)
 
     return True
 
 def exec_command(args):
-    return export_tf(args.db, args.task, args.out)
+    rospy.init_node("export_transform")
+    return export_tf(args.db, args.task, args.out, args.srv)
 
 
 if __name__ == '__main__':
@@ -59,6 +63,10 @@ if __name__ == '__main__':
     # mongodb
     p.add_argument("--db", default="mongodb://localhost:27017/jsk_robot_lifelog/pr1012",
                    type=str, help="address for database")
+
+    # robot_state_publisher
+    p.add_argument("--srv", default="get_transforms",
+                   type=str, help="service name for robot state publisher")
 
     args = p.parse_args()
     sys.exit(exec_command(args))
--- src/jsk_owl_exporter/bson_conversion.py
+++ src/jsk_owl_exporter/bson_conversion.py
@@ -4,10 +4,9 @@
 
 
 import datetime
+import rospy
+from robot_state_publisher.srv import (GetRobotTransforms, GetRobotTransformsRequest)
 
-#######################################
-# rostime
-#######################################
 
 def datetime_to_rostime(dt):
     total = (dt - datetime.datetime.utcfromtimestamp(0)).total_seconds()
@@ -15,60 +14,128 @@ def datetime_to_rostime(dt):
     nsecs = int((total - secs) * 1000000000)
     return { u"secs": secs, u"nsecs": nsecs }
 
-#######################################
-# geometry_msgs/TransformStamped
-#######################################
 
-def transform_stamped_array_to_tf(ts_arr):
-    meta = ts_arr[0].pop("_meta")
-    for ts in ts_arr:
-        if "_meta" in ts:
-            del ts["_meta"]
-        if "_id" in ts:
-            del ts["_id"]
-    return {
-        "_meta": meta,
-        "transforms": ts_arr,
-    }
+class BSONConversion(object):
+    def __init__(self, srv_name="get_transforms"):
+        self.get_transforms_srv = rospy.ServiceProxy(srv_name, GetRobotTransforms)
+    def to_tf_json(self, doc):
+        t = doc["_meta"]["stored_type"]
+        if   t == "posedetection_msgs/Object6DPose":
+            return self.object6dpose_to_tf(doc)
+        elif t == "geometry_msgs/TransformStamped":
+            return self.transform_stamped_to_tf(doc)
+        elif t == "move_base_msgs/MoveBaseActionFeedback":
+            return self.move_base_action_feedback_to_tf(doc)
+        elif t == "control_msgs/FollowJointTrajectoryActionFeedback":
+            return self.follow_joint_trajectory_feedback_to_tf(doc)
+        elif t == "sensor_msgs/JointState":
+            return self.joint_state_to_tf(doc)
 
-def transform_stamped_to_tf(ts):
-    return transform_stamped_array_to_tf([ts])
+    def transform_stamped_array_to_tf(self, ts_arr):
+        meta = ts_arr[0].pop("_meta")
+        for ts in ts_arr:
+            if "_meta" in ts:
+                del ts["_meta"]
+            if "_id" in ts:
+                del ts["_id"]
+        return {
+            "_meta": meta,
+            "transforms": ts_arr,
+        }
 
-#######################################
-# posedetection_msgs/Object6DPose
-#######################################
+    def transform_stamped_to_tf(self, ts):
+        return self.transform_stamped_array_to_tf([ts])
 
-def object6dpose_to_tf(od, robot_frame_id="base_footprint"):
-    return {
-        "transforms": [{
-            "header": {
-                "frame_id": robot_frame_id,
-                "stamp": datetime_to_rostime(od["_meta"]["inserted_at"]),
-                "seq": 0,
-            },
-            "child_frame_id": od["type"],
-            "transform": {
-                "translation": od["pose"]["position"],
-                "rotation": od["pose"]["orientation"],
-            },
+    def object6dpose_to_tf(self, od, robot_frame_id="base_footprint"):
+        return {
             "_meta": od["_meta"],
-        }]}
+            "transforms": [{
+                "header": {
+                    "frame_id": robot_frame_id,
+                    "stamp": datetime_to_rostime(od["_meta"]["inserted_at"]),
+                    "seq": 0,
+                },
+                "child_frame_id": od["type"],
+                "transform": {
+                    "translation": od["pose"]["position"],
+                    "rotation": od["pose"]["orientation"],
+                },
+            }]}
 
-#######################################
-# sensor_msgs/JointState
-#######################################
+    def move_base_action_feedback_to_tf(self, f, robot_frame_id="base_footprint"):
+        base = f["feedback"]["base_position"]
+        return {
+            "_meta": f["_meta"],
+            "transforms": [{
+                "header": base["header"],
+                "child_frame_id": robot_frame_id,
+                "transform": {
+                    "translation": base["pose"]["position"],
+                    "rotation": base["pose"]["orientation"],
+                },
+            }]}
 
-def joint_state_to_tf(js):
-    pass
+    def joint_state_to_tf(self, js):
+        req = GetRobotTransformsRequest()
+        req.joint_states.header.stamp.secs = js["header"]["stamp"]["secs"]
+        req.joint_states.header.stamp.nsecs = js["header"]["stamp"]["nsecs"]
+        req.joint_states.header.seq = js["header"]["seq"]
+        req.joint_states.name = js["name"]
+        req.joint_states.position = js["position"]
+        req.joint_states.velocity = js["velocity"]
+        req.joint_states.effort = js["effort"]
+        res = self.get_transforms_srv(req).transforms
+        return {
+            "_meta": js["_meta"],
+            "transforms": [{
+                "header": {
+                    "seq": t.header.seq,
+                    "frame_id": t.header.frame_id,
+                    "stamp": {
+                        "secs": t.header.stamp.secs,
+                        "nsecs": t.header.stamp.nsecs,
+                    }},
+                "child_frame_id": t.child_frame_id,
+                "transform": {
+                    "translation": {
+                        "x": t.transform.translation.x,
+                        "y": t.transform.translation.y,
+                        "z": t.transform.translation.z },
+                    "rotation": {
+                        "x": t.transform.rotation.x,
+                        "y": t.transform.rotation.y,
+                        "z": t.transform.rotation.z,
+                        "w": t.transform.rotation.w }}} for t in res]}
 
-#######################################
-# move_base_msgs/MoveBaseAction
-#######################################
 
-# TODO
-
-#######################################
-# control_msgs/FollowJointTrajectoryAction
-#######################################
-
-# TODO
+    def follow_joint_trajectory_feedback_to_tf(self, f):
+        fb = f["feedback"]
+        req = GetRobotTransformsRequest()
+        req.joint_states.header.stamp.secs = fb["header"]["stamp"]["secs"]
+        req.joint_states.header.stamp.nsecs = fb["header"]["stamp"]["nsecs"]
+        req.joint_states.header.seq = fb["header"]["seq"]
+        req.joint_states.name = fb["joint_names"]
+        req.joint_states.position = fb["actual"]["positions"]
+        req.joint_states.velocity = fb["actual"]["velocities"]
+        res = self.get_transforms_srv(req).transforms
+        return {
+            "_meta": f["_meta"],
+            "transforms": [{
+                "header": {
+                    "seq": t.header.seq,
+                    "frame_id": t.header.frame_id,
+                    "stamp": {
+                        "secs": t.header.stamp.secs,
+                        "nsecs": t.header.stamp.nsecs,
+                    }},
+                "child_frame_id": t.child_frame_id,
+                "transform": {
+                    "translation": {
+                        "x": t.transform.translation.x,
+                        "y": t.transform.translation.y,
+                        "z": t.transform.translation.z },
+                    "rotation": {
+                        "x": t.transform.rotation.x,
+                        "y": t.transform.rotation.y,
+                        "z": t.transform.rotation.z,
+                        "w": t.transform.rotation.w }}} for t in res]}
